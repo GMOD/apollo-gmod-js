961fbdcf9a87ff2343dacc8f0bd4f037fe24d192
Nathan Dunn <ndunn@me.com>
2020-10-28 16:53:00 -0700

    added test back

--- src/apollo/services/UserService.test.ts
+++ src/apollo/services/UserService.test.ts
@@ -8,24 +8,24 @@ import {loadUsers} from './UserService'
 import {User} from '../domain/User'
 
 test('Load Users', async () => {
-  // const users = await loadUsers()
-  // expect(users.length).toEqual(1)
-  // let user = users[0]
-  // expect(user).toEqual(
-  //   {
-  //     firstName: 'Admin',
-  //     lastName: 'User',
-  //     inactive: false,
-  //     role: 'ADMIN',
-  //     availableGroups: [],
-  //     userCount: 1,
-  //     searchName: null,
-  //     groups: [],
-  //     userId: 49,
-  //     organismPermissions: [],
-  //     username: 'admin@local.host'
-  //   } )
-  // console.log('c')
+  const users = await loadUsers()
+  expect(users.length).toEqual(1)
+  const user = users[0]
+  expect(user).toEqual(
+    {
+      firstName: 'Admin',
+      lastName: 'User',
+      inactive: false,
+      role: 'ADMIN',
+      availableGroups: [],
+      userCount: 1,
+      searchName: null,
+      groups: [],
+      userId: 49,
+      organismPermissions: [],
+      username: 'admin@local.host'
+    } )
+  console.log('c')
 
 })
 
