47703bb15aed59a96441536cc38540103c539c3c
Nathan Dunn <nathandunn@lbl.gov>
2020-10-28 17:38:26 -0700

    Fix 1 travis integration server (#6)
    * tryinng to add server code
    
    * added test back
    
    * added a travis_wait
    
    * adding sleep and caching
    
    * added the sleep timer
    
    * removed additional nodes
    
    * redoing
    
    * forgot to cache the docker images
    
    * running with preoper version of n node now
    
    * updated sleep time
    
    * cleaned up test
    
    * cleaned up and removed the docker caching

--- src/apollo/services/UserService.test.ts
+++ src/apollo/services/UserService.test.ts
@@ -8,24 +8,15 @@ import {loadUsers} from './UserService'
 import {User} from '../domain/User'
 
 test('Load Users', async () => {
-  // const users = await loadUsers()
-  // expect(users.length).toEqual(1)
-  // let user = users[0]
-  // expect(user).toEqual(
-  //   {
-  //     firstName: 'Admin',
-  //     lastName: 'User',
-  //     inactive: false,
-  //     role: 'ADMIN',
-  //     availableGroups: [],
-  //     userCount: 1,
-  //     searchName: null,
-  //     groups: [],
-  //     userId: 49,
-  //     organismPermissions: [],
-  //     username: 'admin@local.host'
-  //   } )
-  // console.log('c')
+  const users = await loadUsers()
+  console.log('returned users',users)
+  expect(users.length).toEqual(1)
+  const user = users[0]
+  expect(user.firstName).toEqual('Admin')
+  expect(user.lastName).toEqual('User')
+  expect(user.inactive).toEqual(false)
+  expect(user.role).toEqual('ADMIN')
+  expect(user.username).toEqual('admin@local.host')
 
 })
 
