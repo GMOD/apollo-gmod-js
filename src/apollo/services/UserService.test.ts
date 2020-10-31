9319427fd9fa1f5875175e7090fe6e700ef3bf62
Nathan Dunn <ndunn@me.com>
2020-10-31 16:21:37 -0700

    updated

--- src/apollo/services/UserService.test.ts
+++ src/apollo/services/UserService.test.ts
@@ -9,7 +9,6 @@ import {User} from '../domain/User'
 test('Load Users', async () => {
   const users = await loadUsers() as Array<User>
   expect(typeof users).not.toEqual('string')
-  // console.log('returned users',users)
   expect(users.length).toBeGreaterThan(0)
   // TODO: add a filter for 'admin@local.host
   const user:User =  users.filter( s => s.username == 'admin@local.host')[0]
@@ -32,18 +31,14 @@ test('Get User', async () => {
 
 test('Delete User', async () => {
   const resultA = await addUser('trash2@bx.psu.edu','Poutrelle','Lapinou') as User
-  console.log('result A',resultA)
   expect(resultA.username).toEqual('trash2@bx.psu.edu')
   const resultB = await getUser('trash2@bx.psu.edu') as User
-  console.log('result B',resultB)
   expect(resultB.username).toEqual('trash2@bx.psu.edu')
   const resultC = await deleteUser('trash2@bx.psu.edu') as User
-  console.log('result C',resultC)
   expect(resultC.username).toEqual('trash2@bx.psu.edu')
   const users = await loadUsers() as Array<User>
-  console.log('add users: ',users)
-  const resultD = await getUser('trash2@bx.psu.edu') as User
-  console.log('result D',resultD)
+  const resultD = await getUser('trash2@bx.psu.edu')
+  expect(resultD).toContain('404')
   expect(resultD.username).not.toEqual('trash2@bx.psu.edu')
 
 })
