491eb9f2d5c9ddad43fd1a11ad9ed6ce17ec6724
Nathan Dunn <ndunn@me.com>
2020-10-31 17:44:24 -0700

    test and cleanup

--- src/apollo/services/UserService.test.ts
+++ src/apollo/services/UserService.test.ts
@@ -38,12 +38,6 @@ test('Delete User', async () => {
   expect(resultB.username).toEqual('trash2@bx.psu.edu')
   const resultC = await deleteUser('trash2@bx.psu.edu') as User
   expect(resultC.username).toEqual('trash2@bx.psu.edu')
-  // console.log('users: ',users)
-  // if(resultD.toString().indexOf('404')<0){
-  //   console.log('doing a delay')
-  //   const delay = (ms:number) => new Promise(res => setTimeout(res, ms))
-  //   await delay(5000)
-  // }
   users = await loadUsers() as Array<User>
   console.log('users 2: ',users)
   let resultD = await getUser('trash2@bx.psu.edu')
