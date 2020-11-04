5345246e9179d04d635b1ddff677dd06185c21be
Nathan Dunn <ndunn@me.com>
2020-11-04 15:11:36 -0800

    fixed render test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,7 +7,7 @@ import App from './App'
 
 test('renders learn react link', async() => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/Web Services/i)
   expect(linkElement).toBeInTheDocument()
 })
 
