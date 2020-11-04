32542f4fbb74085af7cf82423714a6cfce847a31
Nathan Dunn <ndunn@me.com>
2020-11-04 15:12:03 -0800

    small change

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import 'swagger-ui-react/swagger-ui.css'
 
 const App = () => (
   <div className="App">
-    <h3>Web Services</h3>
+    <h3>Apollo 3 Web Services</h3>
     <SwaggerUI
       // url="https://api.mna.dev.spsapps.net/swagger/?format=openapi"
       url="http://localhost:8080/swagger/api/"
