0323e5c5d0119f92c1631353af108a3927368eed
Nathan Dunn <ndunn@me.com>
2020-11-04 14:57:42 -0800

    fixed linting

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,19 @@ import SwaggerUI from 'swagger-ui-react'
 import 'swagger-ui-react/swagger-ui.css'
 
 
-function App() {
-  return (
-    <div className="App">
-      <body>
-        <SwaggerUI
-          // url="https://api.mna.dev.spsapps.net/swagger/?format=openapi"
-          url="http://localhost:8080/swagger/api/"
-          responseInterceptor={response => {
-            if (response.status === 200) {
-              console.log(response.data)
-            }
-            return response // this is what i needed to do.
-          }}
-        />
-      </body>
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <SwaggerUI
+      // url="https://api.mna.dev.spsapps.net/swagger/?format=openapi"
+      url="http://localhost:8080/swagger/api/"
+      responseInterceptor={response => {
+        if (response.status === 200) {
+          console.log(response.data)
+        }
+        return response // this is what i needed to do.
+      }}
+    />
+  </div>
+)
 
 export default App
