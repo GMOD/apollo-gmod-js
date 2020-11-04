3715f6724265a8ba2065ff6004f0119c45116e1d
Nathan Dunn <ndunn@me.com>
2020-11-04 14:42:43 -0800

    added the swagger library to dynamicall add

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import SwaggerUI from 'swagger-ui-react'
+import 'swagger-ui-react/swagger-ui.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <SwaggerUI
+          // url="https://api.mna.dev.spsapps.net/swagger/?format=openapi"
+          url="http://localhost:8080/swagger/api/"
+          responseInterceptor={response => {
+            if (response.status === 200) {
+              console.log(response.data)
+            }
+            return response // this is what i needed to do.
+          }}
+        />
+      </body>
     </div>
   )
 }
