e6c0c4c8e57b77e8a6f685cb68f352c5f0f96bd0
Nathan Dunn <ndunn@me.com>
2020-11-05 10:34:20 -0800

    see if the test works properly with jsdom

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,14 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
 import App from './App'
+import axios from 'axios'
+
+const timer = (ms: any) => new Promise(res => setTimeout(res, ms))
+
+beforeAll(async () =>{
+  const response = await axios.get('http://localhost:8080/swagger/api')
+  const { data } = response
+},30000)
 
 test('renders learn react link', async () => {
   render(<App/>)
@@ -11,3 +19,15 @@ test('renders learn react link', async () => {
   expect(linkElement).toBeInTheDocument()
 })
 
+test('web services available', async () => {
+  const wrapper = render(<App/>)
+  const response = await axios.get('http://localhost:8080/swagger/api', {})
+  const { data } = response
+  expect(data.swagger).toEqual('2.0')
+  const test1 = await screen.getByText(/Apollo 3 Web Services/i)
+  expect(test1).toBeInTheDocument()
+  await timer(2000)
+  const linkElement = await screen.getByText(/Annotation Services/i)
+  wrapper.asFragment()
+  expect(linkElement).toBeInTheDocument()
+})
