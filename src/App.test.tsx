a7b2015f52a892764fe588c71206580a932bd6c7
Nathan Dunn <ndunn@me.com>
2020-11-04 18:56:16 -0800

    fixed test timing and converted to use axios

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,10 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
 import App from './App'
+import axios from 'axios'
+
+const timer = (ms: any) => new Promise(res => setTimeout(res, ms))
+
 
 test('renders learn react link', async () => {
   render(<App/>)
@@ -12,11 +16,14 @@ test('renders learn react link', async () => {
 })
 
 test('web services available', async () => {
-  render(<App/>)
-  const response = await fetch('http://localhost:8080/swagger/api', {})
-  const result = JSON.parse(JSON.stringify(await response.json()))
-
-  expect(result.swagger).toEqual('2.0')
+  const wrapper = render(<App/>)
+  const response = await axios.get('http://localhost:8080/swagger/api', {})
+  const { data } = response
+  expect(data.swagger).toEqual('2.0')
+  const test1 = await screen.getByText(/Apollo 3 Web Services/i)
+  expect(test1).toBeInTheDocument()
+  await timer(1000)
   const linkElement = await screen.getByText(/Annotation Services/i)
+  wrapper.asFragment()
   expect(linkElement).toBeInTheDocument()
 })
