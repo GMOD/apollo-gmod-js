5d290ee302511edd85f91638134632e3143109b9
Nathan Dunn <ndunn@me.com>
2020-11-05 10:24:31 -0800

    this test should pass now

--- src/apollo/services/WebServices.test.tsx
+++ src/apollo/services/WebServices.test.tsx
@@ -4,8 +4,14 @@
 import React from 'react'
 import axios from 'axios'
 
+beforeAll(async () =>{
+  const response = await axios.get('http://localhost:8080/swagger/api')
+  const { data } = response
+},30000)
+
 test('web services available', async () => {
-  const response = await axios.get('http://localhost:8080/swagger/api', {})
+  jest.setTimeout(2000)
+  const response = await axios.get('http://localhost:8080/swagger/api')
   const { data } = response
   expect(data.swagger).toEqual('2.0')
   const paths = Object.keys(data.paths)
