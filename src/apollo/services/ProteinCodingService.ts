887d0946c35e9d128ded6afb3316a6ce68407f09
Nathan Dunn <ndunn@me.com>
2020-11-10 15:01:34 -0800

    fixed compilation error

--- src/apollo/services/ProteinCodingService.ts
+++ src/apollo/services/ProteinCodingService.ts
@@ -1,13 +1,12 @@
 import axios from 'axios'
 import {ProteinCodingTranscript} from '../domain/ProteinCodingTranscript'
+import {ApolloServer} from '../ApolloServer'
 
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 export const addTranscript = async (inputData:JSON): Promise<Array<ProteinCodingTranscript> | string> => {
 
   try {
-    const response = await axios.post( `${ApolloServer.getHost()}/annotationEditor/addTranscript',inputData)
+    const response = await axios.post( `${ApolloServer.getHost()}/annotationEditor/addTranscript`,inputData)
     const { data } = await response
     return data
   } catch (error) {
