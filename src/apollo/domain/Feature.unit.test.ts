4e98eb99ec259b7d23c7ce4feee0ee6b893e5180
Nathan Dunn <nathandunn@lbl.gov>
2021-03-09 11:03:09 -0800

    replaced the remaining uniqueNames

--- src/apollo/domain/Feature.unit.test.ts
+++ src/apollo/domain/Feature.unit.test.ts
@@ -18,7 +18,7 @@ const INPUT_OBJECT = {
       'location': {'fmin': 1235237, 'strand': 1, 'fmax': 1235396},
       'parent_type': {'name': 'mRNA', 'cv': {'name': 'sequence'}},
       'properties': [{'value': 'demo', 'type': {'name': 'owner', 'cv': {'name': 'feature_property'}}}],
-      'uniquename': '@TRANSCRIPT_NAME@',
+      'uniqueName': '@TRANSCRIPT_NAME@',
       'type': {'name': 'exon', 'cv': {'name': 'sequence'}},
       'date_last_modified': 1425583209540,
       'parent_id': '5A8C864885BC71606E120322CE0EC28C'
@@ -28,7 +28,7 @@ const INPUT_OBJECT = {
       'location': {'fmin': 1216824, 'strand': 1, 'fmax': 1216850},
       'parent_type': {'name': 'mRNA', 'cv': {'name': 'sequence'}},
       'properties': [{'value': 'demo', 'type': {'name': 'owner', 'cv': {'name': 'feature_property'}}}],
-      'uniquename': '0992325F0DD2290AB58EA37ECF2DA2E7',
+      'uniqueName': '0992325F0DD2290AB58EA37ECF2DA2E7',
       'type': {'name': 'exon', 'cv': {'name': 'sequence'}},
       'date_last_modified': 1425583209540,
       'parent_id': '5A8C864885BC71606E120322CE0EC28C'
@@ -36,7 +36,7 @@ const INPUT_OBJECT = {
       'location': {'fmin': 1235487, 'strand': 1, 'fmax': 1235616},
       'parent_type': {'name': 'mRNA', 'cv': {'name': 'sequence'}},
       'properties': [{'value': 'demo', 'type': {'name': 'owner', 'cv': {'name': 'feature_property'}}}],
-      'uniquename': '1C091FE87A8133803A69887F38FBDC4C',
+      'uniqueName': '1C091FE87A8133803A69887F38FBDC4C',
       'type': {'name': 'exon', 'cv': {'name': 'sequence'}},
       'date_last_modified': 1425583209542,
       'parent_id': '5A8C864885BC71606E120322CE0EC28C'
@@ -44,7 +44,7 @@ const INPUT_OBJECT = {
       'location': {'fmin': 1224676, 'strand': 1, 'fmax': 1224823},
       'parent_type': {'name': 'mRNA', 'cv': {'name': 'sequence'}},
       'properties': [{'value': 'demo', 'type': {'name': 'owner', 'cv': {'name': 'feature_property'}}}],
-      'uniquename': '6D2E15D6DA759C523B79B96795927CAF',
+      'uniqueName': '6D2E15D6DA759C523B79B96795927CAF',
       'type': {'name': 'exon', 'cv': {'name': 'sequence'}},
       'date_last_modified': 1425583209540,
       'parent_id': '5A8C864885BC71606E120322CE0EC28C'
@@ -52,7 +52,7 @@ const INPUT_OBJECT = {
       'location': {'fmin': 1228682, 'strand': 1, 'fmax': 1228825},
       'parent_type': {'name': 'mRNA', 'cv': {'name': 'sequence'}},
       'properties': [{'value': 'demo', 'type': {'name': 'owner', 'cv': {'name': 'feature_property'}}}],
-      'uniquename': '99C2A027C87DBDBC5536503D5C38F21C',
+      'uniqueName': '99C2A027C87DBDBC5536503D5C38F21C',
       'type': {'name': 'exon', 'cv': {'name': 'sequence'}},
       'date_last_modified': 1425583209540,
       'parent_id': '5A8C864885BC71606E120322CE0EC28C'
@@ -60,14 +60,14 @@ const INPUT_OBJECT = {
       'location': {'fmin': 1216824, 'strand': 1, 'fmax': 1235534},
       'parent_type': {'name': 'mRNA', 'cv': {'name': 'sequence'}},
       'properties': [{'value': 'demo', 'type': {'name': 'owner', 'cv': {'name': 'feature_property'}}}],
-      'uniquename': '994B96C6594F5DB1B6C836E6E0EDE2A6',
+      'uniqueName': '994B96C6594F5DB1B6C836E6E0EDE2A6',
       'type': {'name': 'CDS', 'cv': {'name': 'sequence'}},
       'date_last_modified': 1425583209540,
       'parent_id': '5A8C864885BC71606E120322CE0EC28C'
     }
     ],
     'properties': [{'value': 'demo', 'type': {'name': 'owner', 'cv': {'name': 'feature_property'}}}],
-    'uniquename': '5A8C864885BC71606E120322CE0EC28C',
+    'uniqueName': '5A8C864885BC71606E120322CE0EC28C',
     'type': {'name': 'mRNA', 'cv': {'name': 'sequence'}},
     'date_last_modified': 1425583209602,
     'parent_id': '8B9E9AC4D0DB90464F26B2F77A1E09B4'
