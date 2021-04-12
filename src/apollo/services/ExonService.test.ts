3fcd53d009ccdc27d4b91f0ffc90ff31c3f7815c
Nathan Dunn <nathandunn@lbl.gov>
2021-04-12 09:16:53 -0700

    added real data test

--- src/apollo/services/ExonService.test.ts
+++ src/apollo/services/ExonService.test.ts
@@ -45,15 +45,19 @@ test('Create 2 exons on a transcript and delete one, confirm boundaries mapped',
     'organism': TEST_ORGANISM,
     'sequence': 'Group1.10',
     'features': [{
-      'location': {'fmin': 2, 'fmax': 25, 'strand': 1},
+      'location': {'fmin': 19636, 'fmax': 31167, 'strand': -1},
       'type': {'cv': {'name': 'sequence'}, 'name': 'mRNA'},
-      'name': 'GB40856-RA',
+      'name': 'GB40789-RA',
+      'orig_id': 'GB40789-RA',
       'children': [{
-        'location': {'fmin': 5, 'fmax': 10, 'strand': 1},
+        'location': {'fmin': 19636, 'fmax': 20199, 'strand': -1},
         'type': {'cv': {'name': 'sequence'}, 'name': 'exon'}
       }, {
-        'location': {'fmin': 15, 'fmax': 20, 'strand': 1},
+        'location': {'fmin': 30857, 'fmax': 31167, 'strand': -1},
         'type': {'cv': {'name': 'sequence'}, 'name': 'exon'}
+      }, {
+        'location': {'fmin': 19636, 'fmax': 31167, 'strand': -1},
+        'type': {'cv': {'name': 'sequence'}, 'name': 'CDS'}
       }]
     }]
   }
@@ -62,29 +66,29 @@ test('Create 2 exons on a transcript and delete one, confirm boundaries mapped',
   console.log(JSON.stringify(returnObject))
   const returnGenomeAnnotationGroup = new GenomeAnnotationGroup(returnObject)
   expect(returnGenomeAnnotationGroup.features.length).toEqual(1)
-  console.log('return features',returnGenomeAnnotationGroup.features[0].children)
+  console.log('return features', returnGenomeAnnotationGroup.features[0].children)
   const returnFeature = returnGenomeAnnotationGroup.features[0]
-  expect(returnFeature.name).toEqual('GB40856-RA-00001')
+  expect(returnFeature.name).toEqual('GB40789-RA-00001')
   // 2 exons, 1 CDS, 2 non-canonical five-prime splice sites, 1 non-canonical three-prime splice sites
-  expect(returnFeature.children.length).toEqual(6)
+  expect(returnFeature.children.length).toEqual(3)
 
 
   // 3. get features on sequence (should be this one)
   const annotationsFoundResponse1 = await annotationEditorCommand(getFeaturesCommand, 'getFeatures')
-  console.log('annotationsFoundResponse1',JSON.stringify(annotationsFoundResponse1))
+  console.log('annotationsFoundResponse1', JSON.stringify(annotationsFoundResponse1))
   const genomeAnnotationFound1 = new GenomeAnnotationGroup(annotationsFoundResponse1)
+  console.log('genomeAnnotationFound1', JSON.stringify(genomeAnnotationFound1))
   expect(genomeAnnotationFound1.features.length).toEqual(1)
   const addedFeature1 = genomeAnnotationFound1.features[0]
-  expect(addedFeature1.location?.fmin).toEqual(2)
-  expect(addedFeature1.location?.fmax).toEqual(25)
-  expect(addedFeature1.children?.length).toEqual(6)
+  expect(addedFeature1.location?.fmin).toEqual(19636)
+  expect(addedFeature1.location?.fmax).toEqual(31167)
+  expect(addedFeature1.children?.length).toEqual(3)
   expect(addedFeature1.uniqueName).toBeDefined()
 
   // delete the right exon . . . S
   const rightExon = addedFeature1.children[2]
-  console.log('delete right exon',rightExon)
+  console.log('delete right exon', rightExon)
   const uniqueNameToDelete = rightExon.uniqueName
-
 
 
   // 4. delete feature
@@ -106,7 +110,9 @@ test('Create 2 exons on a transcript and delete one, confirm boundaries mapped',
   expect(addedFeature1.location?.fmax).toEqual(25)
   expect(addedFeature1.children?.length).toEqual(3)
 
-}, 1000)
+},
+1000
+)
 
 
 beforeAll(async () => {
