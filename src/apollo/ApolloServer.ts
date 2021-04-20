b56d3dd8e4654764b5fc3955b7af46aba8525191
Nathan Dunn <nathandunn@lbl.gov>
2021-04-20 09:57:08 -0700

    user service works

--- src/apollo/ApolloServer.ts
+++ src/apollo/ApolloServer.ts
@@ -2,7 +2,7 @@
 export class ApolloServer{
 
   static getHost():string{
-    return process.env.APOLLO_URL || 'http://localhost:8080'
+    return process.env.APOLLO_URL || 'http://localhost:8080/apollo'
   }
 
 }
--- src/apollo/services/UserService.test.ts
+++ src/apollo/services/UserService.test.ts
@@ -7,49 +7,50 @@ import {addUser, deleteUser, getUser, loadUsers} from './UserService'
 import {User} from '../domain/User'
 import {sleep} from '../functions/Timing'
 
+const TEST_USER = 'admin@local.host'
+const TEST_PASS = 'password'
+
 test('Load Users', async () => {
-  const users = await loadUsers() as Array<User>
+  const users = await loadUsers(TEST_USER,TEST_PASS) as Array<User>
   expect(typeof users).not.toEqual('string')
   expect(users.length).toBeGreaterThan(0)
   // TODO: add a filter for 'admin@local.host
-  const user:User =  users.filter( s => s.username == 'admin@local.host')[0]
-  expect(user.firstName).toEqual('Admin')
-  expect(user.lastName).toEqual('User')
+  const user:User =  users.filter( s => s.username == TEST_USER)[0]
+  expect(user.firstName).toEqual('Ad')
+  expect(user.lastName).toEqual('min')
   expect(user.inactive).toEqual(false)
   expect(user.role).toEqual('ADMIN')
-  expect(user.username).toEqual('admin@local.host')
+  expect(user.username).toEqual(TEST_USER)
 
 })
 
 test('Get User', async () => {
-  const user = await getUser('admin@local.host') as User
+  const user = await getUser(TEST_USER,TEST_USER,TEST_PASS) as User
   expect(typeof user).not.toEqual('string')
-  expect(user.firstName).toEqual('Admin')
-  expect(user.lastName).toEqual('User')
-  expect(user.username).toEqual('admin@local.host')
+  expect(user.firstName).toEqual('Ad')
+  expect(user.lastName).toEqual('min')
+  expect(user.username).toEqual(TEST_USER)
 
 })
 
 test('Delete User', async () => {
-  const resultA = await addUser('trash2@bx.psu.edu','Poutrelle','Lapinou') as User
+  const hasUser = await getUser('trash2@bx.psu.edu',TEST_USER,TEST_PASS) as User
+  const resultA = await addUser('trash2@bx.psu.edu','topsecret','Poutrelle','Lapinou',TEST_USER,TEST_PASS) as User
   expect(resultA.username).toEqual('trash2@bx.psu.edu')
-  let users = await loadUsers() as Array<User>
   await sleep(500)
-  const resultB = await getUser('trash2@bx.psu.edu') as User
+  const resultB = await getUser('trash2@bx.psu.edu',TEST_USER,TEST_PASS) as User
   expect(resultB.username).toEqual('trash2@bx.psu.edu')
-  const resultC = await deleteUser('trash2@bx.psu.edu') as User
+  const resultC = await deleteUser('trash2@bx.psu.edu',TEST_USER,TEST_PASS) as User
   expect(resultC.username).toEqual('trash2@bx.psu.edu')
-  users = await loadUsers() as Array<User>
-  let resultD = await getUser('trash2@bx.psu.edu')
-  resultD = await getUser('trash2@bx.psu.edu')
+  const resultD = await getUser('trash2@bx.psu.edu',TEST_USER,TEST_PASS)
   expect(resultD).toBeUndefined()
 
 })
 
 beforeEach( async () => {
-  await deleteUser('trash2@bx.psu.edu')
+  await deleteUser('trash2@bx.psu.edu',TEST_USER,TEST_PASS)
 })
 
 afterEach( async () => {
-  await deleteUser('trash2@bx.psu.edu')
+  await deleteUser('trash2@bx.psu.edu',TEST_USER,TEST_PASS)
 })
