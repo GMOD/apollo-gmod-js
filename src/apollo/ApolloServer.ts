a3371e0f632cd0bb92384d356ec448e4ca67ad3a
Nathan Dunn <nathandunn@lbl.gov>
2021-04-21 20:20:13 -0700

    added debugging code

--- src/apollo/ApolloServer.ts
+++ src/apollo/ApolloServer.ts
@@ -2,7 +2,10 @@
 export class ApolloServer{
 
   static getHost():string{
-    return process.env.APOLLO_URL || 'http://localhost:8080/apollo'
+    console.log('getting host ',process.env.APOLLO_URL)
+    const output = process.env.APOLLO_URL || 'http://localhost:8080/apollo'
+    console.log('output: ',output)
+    return output
   }
 
 }
